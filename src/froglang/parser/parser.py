fe0893eabfd2036e485effc4c9acef19f2aca784
RedoC-github <coderlovemedia@gmail.com>
2020-09-20 11:51:39 +0900

    :construction: made basic parser class structure

--- src/froglang/parser/parser.py
+++ src/froglang/parser/parser.py
@@ -4,3 +4,27 @@ The Frog Programming Language Official Parser
 
 Development Leader: @RedoC
 """
+
+import src.froglang.grammar.operations as oper
+import src.froglang.grammar.keywords as keywd
+
+
+class Parser:
+    """
+    class Parser parse frogcode to bytecode
+    """
+
+    def __init__(self, linecode):
+        """
+        self.__init__(linecode)
+        init class
+        :param linecode:
+        """
+        self.linecode = linecode
+
+    def parse(self):
+        """
+        self.parse()
+        parsing main method
+        :return:
+        """
